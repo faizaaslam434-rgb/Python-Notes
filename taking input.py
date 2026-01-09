00a3b2f101df05c739834923a82f0de3c9b326eb
SYEDA FAIZA ASLAM <faizaaslam434@gmail.com>
2026-01-10 00:59:45 +0500

    Add files via upload

---
+++ taking input.py
@@ -0,0 +1,17 @@
+
+name=input("enter name: ")
+#print("hello",name)
+age=input("enter age: ")
+marks=input("enter marks: ")
+print("hello",name)
+print("age",age)
+print("marks",marks)
+num1=int(input("Enter 1st number: "))
+num2=int(input("Enter 2nd number: "))
+sum=num1+num2
+avg=(sum)/2
+print("Sum of 2 number is: ",sum)
+print("average is: ",avg)
+side=float(input("Enter side of square: "))
+area=side*side
+print("Area of square is: ",area)
