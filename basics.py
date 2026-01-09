c2185e334af42fc948b1f6c417256402dad8931e
SYEDA FAIZA ASLAM <faizaaslam434@gmail.com>
2026-01-10 00:56:35 +0500

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,13 @@
+print("hellow world")
+print("i know how to code")
+name = "Faiza"
+print("name")
+print(name)
+age = 17
+print(age)
+name = input("Enter your name:")
+print("hello" + name)
+
+old_age = input("enter your old age: ")
+new_age = int(old_age) + 2
+print(new_age)
