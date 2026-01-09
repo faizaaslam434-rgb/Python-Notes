835660d10aa660009b9323bcb2ef7e5b4c288095
SYEDA FAIZA ASLAM <faizaaslam434@gmail.com>
2026-01-10 00:58:58 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,11 @@
+name = "Faiza Aslam"
+#print upper and lower
+print(name.upper())
+print(name.lower())
+#print index of find characterf or string
+print(name.find('A'))
+#print replace string
+print(name.replace("Faiza Aslam","Amaan Khan"))
+#print True or false if character or string is present
+print("s"in name)
+print('Y'in name)
