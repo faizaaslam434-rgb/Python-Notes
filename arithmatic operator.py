a37db9dde633fd805d3774bd2bf8fcb3e4a695dd
SYEDA FAIZA ASLAM <faizaaslam434@gmail.com>
2026-01-10 00:56:14 +0500

    Add files via upload

---
+++ arithmatic operator.py
@@ -0,0 +1,14 @@
+#add
+print(3+4)
+#subtract
+print(3-4)
+#multiply
+print(3*4)
+#divide
+print(10/5)
+#modulus
+print(3%4)
+# double divide remove the decimal point values
+print(10//5)
+#double asterisk is used to the a to the power b
+print( 4**3)  #means 4x4x4
